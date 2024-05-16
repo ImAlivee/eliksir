b073a5c2c4ee9b5d6d417de4bd2b81cdde493097
olztan <tom85.roche@gmail.com>
2024-05-16 17:45:39 +0200

    liens header fonctionnels

--- app/view/common/header.php
+++ app/view/common/header.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$page_title?></title>
-    <link rel="stylesheet" href="public/css/style.css">
-    <link rel="stylesheet" href="grid.css">
-    <link rel="stylesheet" href="taille_img.css">
+    <link rel="stylesheet" href="/public/css/style.css">
+    <link rel="stylesheet" href="/public/css/grid.css">
+    <link rel="stylesheet" href="/public/css/main.css">
     <link rel="icon" type="image/svg+xml" href="/public/images/favicon/favicon.svg">
 
 </head>
