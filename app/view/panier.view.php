afdc3a98563c367f392ec8db9f89006b12299983
ImAlivee <144893180+ImAlivee@users.noreply.github.com>
2024-05-16 17:23:26 +0200

    update panier.view

---
+++ app/view/panier.view.php
@@ -0,0 +1,15 @@
+<h1>Panier</h1>
+<section>
+<h2>produits</h2>
+<h2>quantité</h2>
+<h2>prix total</h2>
+<p>nom</p>
+<form action="" method="get">
+    <input type="number" name="comment">
+</form>
+<p id="prix">11.00€</p>
+</section>
+<section class="">Total du panier</section>
+<button type="submit">poursuivre les achats</button>
+<button type="submit">commander</button>
+   
