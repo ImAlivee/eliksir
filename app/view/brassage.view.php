ac102fef868a03e6687783d795ee0224c6f0de37
olztan <tom85.roche@gmail.com>
2024-05-16 09:23:07 +0200

    Passation en MCV de Brassage.php

--- app/view/brassage.view.php
+++ app/view/brassage.view.php
@@ -1,28 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="brassage.css">
-    <link rel="stylesheet" href="grid.css">
-    <link rel="stylesheet" href="taille_img.css">
-    <link rel="icon" type="image/svg+xml" href="img_brassage_site_web/favicon.svg">
-
-</head>
-<body>
-    <nav>
-        <img id="logo" src="img_brassage_site_web/logo_eliksir_beige.png" alt="" href="">
-        <ul>
-            <li>Les produits</li>
-            <li>Le brassage</li>
-            <li>Qui sommes nous ?</li>
-            <li>Les produits</li>
-            <li>Contacts</li>
-            <li><img id="bouton_panier" src="img_brassage_site_web/Panier.png" alt="" href=" "></li>
-        </ul>
-    </nav>
-    <main>
         <h1 id="intro">Comment sont fabriqué les bières chez ELIKSIR ?</h1>
         <section class="sommaire">
         <ul>
@@ -132,9 +107,4 @@
             </li>
            
         </section>
-    </main>
-    <footer>
 
-    </footer>
-</body>
-</html>
