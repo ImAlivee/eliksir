a8c9bff9435e363c916385292fd0673db778a1b0
Jiciju <muller_mathis@yahoo.fr>
2024-05-16 17:27:36 +0200

    Update produits.view.php

---
+++ app/view/produits.view.php
@@ -0,0 +1,11 @@
+<main> 
+    <ul class="listebiere">
+        <li>  <!--A faire en boucle en fonction du nombre de bière-->
+            <h2>Nom de la bière</h2>
+            <img src="">
+            <h3>Prix</h3>
+            <p>Liste des ingrédients</p>
+            <h3>Pourcentage d'alcool</h3>
+        </li>
+    </ul>
+</main>
