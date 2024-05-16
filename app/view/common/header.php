f9ad4a35c04254b275f3dd36cdf762227826965a
ImAlivee <144893180+ImAlivee@users.noreply.github.com>
2024-05-16 09:06:32 +0200

    create heaer.php

---
+++ app/view/common/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="brassage.css">
+    <link rel="stylesheet" href="grid.css">
+    <link rel="stylesheet" href="taille_img.css">
+    <link rel="icon" type="image/svg+xml" href="img_brassage_site_web/favicon.svg">
+
+</head>
+<body>
+    <nav>
+        <img id="logo" src="img_brassage_site_web/logo_eliksir_beige.png" alt="" href="">
+        <ul>
+            <li>Les produits</li>
+            <li>Le brassage</li>
+            <li>Qui sommes nous ?</li>
+            <li>Les produits</li>
+            <li>Contacts</li>
+            <li><img id="bouton_panier" src="img_brassage_site_web/Panier.png" alt="" href=" "></li>
+        </ul>
+    </nav>
+    <main>
