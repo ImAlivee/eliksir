5812846bf4a23c32dbfba9a5779b2e9920c798a4
Nawfal17 <89976354+Nawfal17@users.noreply.github.com>
2024-05-23 16:04:13 +0200

    header tom

--- app/view/common/header.php
+++ app/view/common/header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$page_title?></title>
     <link rel="stylesheet" href=<?=$css?>>
-    <link rel="stylesheet" href="/public/css/main.css">
+    <link rel="stylesheet" type="text/css" href="/public/css/main.css">
     <link rel="icon" type="image/svg+xml" href="/public/images/favicon/favicon.svg">
 
 </head>
