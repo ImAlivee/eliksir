f0b248401af6219d29b4b24622418abd2a64e4df
Hnry77176 <hm77176@icloud.com>
2024-05-30 10:44:39 +0200

    try image

--- app/view/panier.view.php
+++ app/view/panier.view.php
@@ -2,7 +2,7 @@
 <section class="grid">
     <div class="gridleft">
         <h2 id="child" class="line1">produits</h2>
-        <img class="biere" src="/public/images/img_brassage_site_web/Posts_bieres/Posts bières/heritage.jpg">
+        <img class="biere" src="public/images/img_brassage_site_web/Posts_bieres/Posts bières/heritage.jpg">
         <p>nom</p>
     </div>
     <div class="gridright">
@@ -18,3 +18,11 @@
 </section>
 <button type="submit">poursuivre les achats</button>
 <button type="submit">commander</button>
+
+<div class="suggestion">
+    <img id="photo" src="public/images/img_brassage_site_web/Posts_bieres/Posts bières/richesse.jpg" alt="">
+    <h3>Nom de la biere</h3>
+    <p>Type de biere</p>
+</div>
+</body>
+</html>
