cb54e56fa3c4c00a357d4a0a92e3646d66e5b2fa
Hnry77176 <hm77176@icloud.com>
2024-06-03 15:12:30 +0200

    accueil

---
+++ app/view/accueil.view.php
@@ -0,0 +1,23 @@
+<div class="heritage">
+    <img src="public\images\img_brassage_site_web\Posts_bieres\Heritage.jpg" alt="">
+    <p>texte</p>
+</div>
+
+<div class="suggestion">
+    <img src="public\images\img_brassage_site_web\Posts_bieres\resilience.jpg" alt="">
+    <img src="public\images\img_brassage_site_web\Posts_bieres\sante.jpg" alt="">
+    <img src="public\images\img_brassage_site_web\Posts_bieres\seduction.jpg" alt="">
+</div>
+
+<div class="etape">
+    <h1>Etape</h1>
+    <input class="bouton" type="button" value="En savoir plus">
+</div>
+<p>Texte de description pour les etapes</p>
+
+<div class="nous">
+    <p>texte a description</p>
+    <img src="public\images\photos_groupe\photo_groupe.jpg" alt="">
+    <p>texte a description</p>
+</div>
+<input class="bouton2" type="button" value="En savoir plus">
