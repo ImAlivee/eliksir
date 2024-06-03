4fa4b7b1b7aa0c995d30e95382511ab9f1eb9699
Jiciju <muller_mathis@yahoo.fr>
2024-06-03 14:11:47 +0200

    Update produits.view.php
    suppression

--- app/view/produits.view.php
+++ app/view/produits.view.php
@@ -1,47 +1,11 @@
-<?php
-// Création du tableau de bières
-$bieres = array(
-    "bière1" => array(
-        "nom" => "nom1",
-        "prix" => 2.99,
-        "taille" => "taille1"
-    ),
-    "bière2" => array(
-        "nom" => "nom2",
-        "prix" => 2.99,
-        "taille" => "taille2"
-    ),
-    "bière3" => array(
-        "nom" => "nom3",
-        "prix" => 2.99,
-        "taille" => "taille3"
-    ),
-    "bière4" => array(
-        "nom" => "nom4",
-        "prix" => 2.99,
-        "taille" => "taille4"
-    ),
-    "bière5" => array(
-        "nom" => "nom5",
-        "prix" => 2.99,
-        "taille" => "taille5"
-    ),
-    "bière6" => array(
-        "nom" => "nom6",
-        "prix" => 2.99,
-        "taille" => "taille6"
-    )
-);
-
-?>
 
 <ul class="liste-de-bière">
         <?php foreach ($bieres as $type => $listeBieres): ?>
             <?php foreach ($listeBieres as $biere): ?>
                 <li>
-                    <h3><?php echo $biere["nom"]; ?></h3>
-                    <p>Prix: <?php echo $biere["prix"]; ?>€</p>
-                    <p>Taille: <?php echo $biere["taille"]; ?></p>
+                    <h3><?php echo $bieres["nom"]; ?></h3>
+                    <p>Prix: <?php echo $bieres["prix"]; ?>€</p>
+                    <p>Taille: <?php echo $bieres["taille"]; ?></p>
                 </li>
             <?php endforeach; ?>
         <?php endforeach; ?>
