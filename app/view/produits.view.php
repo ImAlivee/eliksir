9b01b600ca88324315089153c380eb94ccd62ac7
Jiciju <muller_mathis@yahoo.fr>
2024-06-04 13:19:31 +0200

    ajout image produit

--- app/view/produits.view.php
+++ app/view/produits.view.php
@@ -3,7 +3,7 @@
     <ul class="listebiere">
         <li>
             <h2><?php echo $biere["nom_produit"]; ?></h2>
-            <img src="">
+            <img src="public\images\img_brassage_site_web\Posts_bieres\<?php echo $biere["image"];?>">
             <h3><?php echo $biere["prix"]; ?></h3>
             <h3><?php echo $biere["type"]; ?></h3>
             <h3><?php echo $biere["pourcentage_alcool"]; ?></h3>
