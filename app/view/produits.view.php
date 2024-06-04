11581381a4e895947b274d8a8e30c0402ea9e902
Jiciju <muller_mathis@yahoo.fr>
2024-06-04 12:39:32 +0200

    zeufhqubhfjsd

--- app/view/produits.view.php
+++ app/view/produits.view.php
@@ -5,7 +5,7 @@
             <h2><?php echo $biere["nom_produit"]; ?></h2>
             <img src="">
             <h3><?php echo $biere["prix"]; ?></h3>
-            <p><?php echo $biere["quantite"]; ?></p>
+            <h3><?php echo $biere["type"]; ?></h3>
             <h3><?php echo $biere["pourcentage_alcool"]; ?></h3>
         </li>
         <li>
