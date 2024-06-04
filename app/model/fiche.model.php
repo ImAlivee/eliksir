c214423b1c8835d0273ff61c0e0b55c0f229a6a1
olztan <tom85.roche@gmail.com>
2024-06-04 17:01:52 +0200

    again...

--- app/model/fiche.model.php
+++ app/model/fiche.model.php
@@ -1,9 +1,9 @@
 <?php
 
 function getBeer(int $numBeer, PDO $pdo): array {
-    $sql="SELECT * FROM biere WHERE id_produits=:id";
+    $sql="SELECT * FROM biere WHERE id_produits=:num";
     $stmt= $pdo -> prepare($sql);
-    $stmt -> bindParam(':id', $numBeer, PDO::PARAM_INT);
+    $stmt -> bindParam(':num', $numBeer, PDO::PARAM_INT);
     $stmt -> execute();
     if (!$biere = $stmt -> fetch()) {
         $_SESSION['message'] = "L'identifiant de l'étudiant n'est pas correct.";
