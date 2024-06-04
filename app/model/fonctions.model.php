1c8db1853e5d5c2c2646887451453101be648b14
ImAlivee <144893180+ImAlivee@users.noreply.github.com>
2024-06-04 12:54:16 +0200

    update model

--- app/model/fonctions.model.php
+++ app/model/fonctions.model.php
@@ -21,3 +21,11 @@ function totalCart($totalItem){
         }
     }
 }
+function getInfo(PDO $pdo): array
+{
+    $sql = "SELECT * FROM biere";
+    $stmt = $pdo->query($sql);
+    $infos = $stmt->fetchAll();
+
+    return $infos;
+}
