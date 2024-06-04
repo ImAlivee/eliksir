62b7232f45ab26970422d61db8200b61043b720a
olztan <tom85.roche@gmail.com>
2024-06-04 17:45:35 +0200

    pas çaaaaaaa

--- app/view/produits.view.php
+++ app/view/produits.view.php
@@ -2,7 +2,7 @@
     <?php foreach ($bieres as $biere): ?>
         <ul class="listebiere">
             <li>
-                <a href="fiche_produit.php?num=<?= $biere['id_produit'] ?>">
+                <a href="fiche_produit.php?num=<?= $biere['id_produit']; ?>">
                 <h2><?php echo $biere["nom_produit"]; ?></h2>
                 <img src="public\images\img_brassage_site_web\Posts_bieres\<?php echo $biere["image"];?>">
                 <h3><?php echo $biere["prix"]; ?></h3>
