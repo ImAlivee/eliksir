c05a5f6c4ef8f0e1bad5de35299afb0727600865
ImAlivee <144893180+ImAlivee@users.noreply.github.com>
2024-06-04 12:14:11 +0200

    commit

--- app/view/produits.view.php
+++ app/view/produits.view.php
@@ -1,17 +1,15 @@
-<ul class="liste-de-bière">
-        <?php foreach ($bière as $bières): ?>
+<ul class="liste-de-biere">
+        <?php foreach ($bieres as $biere): ?>
     <ul class="listebiere">
         <li>
             <h2><?php echo $biere["nom_produit"]; ?></h2>
             <img src="">
             <h3><?php echo $biere["prix"]; ?></h3>
-            <p><?php echo $biere["quantité"]; ?></p>
+            <p><?php echo $biere["quantite"]; ?></p>
             <h3><?php echo $biere["pourcentage_alcool"]; ?></h3>
         </li>
-        <?php endforeach; ?>
-        <?php foreach ($bière as $id_produit => $nom_produit) : ?>
-            <li>
-                <a href="panier.php?action=add&id=<?php echo $id_produit; ?>">Ajouter au panier</a>
+        <li>
+                <a href=" ">Ajouter au panier</a>
             </li>
-        <?php endforeach ?>
+        <?php endforeach; ?>
 </ul>
